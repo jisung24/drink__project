4c75d61bf34e0d73d73efb8bb7252ad094e2d463
jisung24 <jisung4012@naver.com>
2022-11-24 19:34:53 +0900

    md file 공통된거 삭제

--- app.js
+++ app.js
@@ -35,7 +35,4 @@ app.use('/api/auth', authRouter(passport(app)));
 app.use('/drink-shop', shopRouter());
 app.use('/test', testRouter());
 
-// app.get('/', async(req,res) => {
-//     res.render('testPage.ejs');
-// })
 module.exports = app;
